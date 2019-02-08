7ee5d662e2a5cd563d8e9694251862b5a2f5f639
Bram Jetten <mail@bramjetten.nl>
2019-02-08 16:32:11 +0100

    Helpers

--- app/helpers/spina/pos/application_helper.rb
+++ app/helpers/spina/pos/application_helper.rb
@@ -1,6 +1,17 @@
 module Spina
   module Pos
-    module ApplicationHelper
+    module PosHelper
+
+      def image_variant(image, variant, fallback: nil)
+        if image&.file&.attached?
+          main_app.url_for(image.file.variant(variant))
+        else
+          fallback || ""
+        end
+      rescue
+        ""
+      end
+      
     end
   end
 end
